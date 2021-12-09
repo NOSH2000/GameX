d6670af9ef07bd8381bfea6c247eb9959a007bb6
Nowshin Sayara <sayara@Nowshins-MacBook-Pro.local>
2021-12-09 15:11:06 -0500

    Edit player page

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+// https://codepen.io/jamesqquick/pen/XWJxBQv
+
 const searchBar = document.getElementById("searchBar"); // Gets the reference to the search bar
 const gameList = ['Dota 2', 'Battlefield 2042', 'League Of Legends', 'Valorant', 'World of Warcraft', 'CS-GO', 'Fortnite', 'World of Tanks'];
 const charactersList = document.getElementById('charactersList');
-const GameSearchBar = document.getElementById('searchBar');
+const gameSearchBar = document.getElementById('searchBar');
 let proPlayers = [];
 
 const resizeBrowser = () => {
@@ -19,48 +21,43 @@ searchBar.addEventListener('keyup', (e) => {
   const filteredGame = gameList.filter(gameName => {
     return gameName.toLowerCase().includes(searchString);
   });
-  console.log(filteredGame);
 });
 
-
-GameSearchBar.addEventListener('keyup', (e) => {
-  const searchString = e.target.value.toLowerCase();
-
-  const filteredCharacters = hpCharacters.filter((character) => {
-    return (
-      character.name.toLowerCase().includes(searchString) ||
-      character.house.toLowerCase().includes(searchString)
-    );
+gameSearchBar.addEventListener('keyup', (e) => {
+    const searchString = e.target.value.toLowerCase();
+    const filteredPlayers = proPlayers.filter((player) => {
+      return (
+        player.name.toLowerCase().includes(searchString) ||
+        player.steamid.toLowerCase().includes(searchString)
+      );
+    });
+    console.log(filteredPlayers);
+    displayPlayers(filteredPlayers);
   });
-  displayCharacters(filteredCharacters);
-});
-
-
-// https://codepen.io/jamesqquick/pen/XWJxBQv
-const loadPlayers = async () => {
-  try {
-    const res = await fetch('https://api.opendota.com/api/proPlayers');
-    proPlayers = await res.json();
-    displayPlayers(proPlayers);
-  } catch (err) {
-    console.error(err);
-  }
-};
-
-const displayPlayers = (players) => {
-  const htmlString = players
-
-    .map((player) => {
-      return `
-          <li class="player">
-              <a target='_blank' href=${player.profileurl}><h2>${player.name}</h2></a>
-              <p>SteamID: ${player.steamid}</p>
-              <img src="${player.avatarfull}"></img>
-          </li>
-      `;
-    })
-    .join('');
-  charactersList.innerHTML = htmlString;
-};
-
-loadPlayers();
+  
+  const loadPlayers = async () => {
+    try {
+      const res = await fetch('https://api.opendota.com/api/proPlayers');
+      proPlayers = await res.json();
+      displayPlayers(proPlayers);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  
+  const displayPlayers = (players) => {
+    const htmlString = players
+      .map((player) => {
+        return `
+            <li class="player">
+                <a target='_blank' href=${player.profileurl}><h2>${player.name}</h2></a>
+                <p>SteamID: ${player.steamid}</p>
+                <img src="${player.avatarfull}"></img>
+            </li>
+        `;
+      })
+      .join('');
+      playersList.innerHTML = htmlString;
+  };
+  
+  loadPlayers();
