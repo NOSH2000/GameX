820ff984782ea6915a2eebf47d0b4404d714ac16
Nowshin Sayara <sayara@Nowshins-MacBook-Pro.local>
2021-12-08 21:37:17 -0500

    Add responsive menubar functionality. only works for index.html

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const searchBar = document.getElementById("searchBar"); // Gets the reference to the search bar
-const gameList = ['Ddota 2', 'Battlefield 2042', 'League Of Legends', 'Valorant', 'World of Warcraft', 'CS-GO', 'Fortnite', 'World of Tanks'];
+const gameList = ['Dota 2', 'Battlefield 2042', 'League Of Legends', 'Valorant', 'World of Warcraft', 'CS-GO', 'Fortnite', 'World of Tanks'];
 
 // keyUp -> anytime a key is pressed
 searchBar.addEventListener('keyup', (e) => {
-    const searchString = e.target.value.toLowerCase();
-    const filteredGame = gameList.filter( gameName => {
-        return gameName.toLowerCase().includes(searchString);
-    });
-    console.log(filteredGame);
+  const searchString = e.target.value.toLowerCase();
+  const filteredGame = gameList.filter(gameName => {
+    return gameName.toLowerCase().includes(searchString);
+  });
+  console.log(filteredGame);
 });
+
+const resizeBrowser = () => {
+  let x = document.getElementById("myTopnav");
+  if (x.className === "navbarlinks") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbarlinks";
+  }
+}
